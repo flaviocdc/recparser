f55fbadc55d8f249ef7600bb130e0ed9a2e895e1
Flavio Costa <flavio.cdc@gmail.com>
2011-07-28 19:41:16 -0300

    Melhorando estrutura de diretorios

--- monga.sh
+++ monga.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
 OUTPUT_DIR=tmp-comp
+
+TARGET=$(basename $1)
+OUTPUT_FILE=gen/${TARGET%.*}
+
 mkdir -p $OUTPUT_DIR
 
-./cfg-llvm $1 > $OUTPUT_DIR/$1.llvm
-llc $OUTPUT_DIR/$1.llvm
-as -o $OUTPUT_DIR/$1.o $OUTPUT_DIR/$1.llvm.s
+./cfg-llvm $1 > $OUTPUT_DIR/$TARGET.llvm
+llc $OUTPUT_DIR/$TARGET.llvm
+as -o $OUTPUT_DIR/$TARGET.o $OUTPUT_DIR/$TARGET.llvm.s
 
 gcc -c runtime.c -o $OUTPUT_DIR/runtime.o
-gcc -o $1.prog $OUTPUT_DIR/runtime.o $OUTPUT_DIR/$1.o
+gcc -o $OUTPUT_FILE $OUTPUT_DIR/runtime.o $OUTPUT_DIR/$TARGET.o
 
 rm -rf $OUTPUT_DIR
 
